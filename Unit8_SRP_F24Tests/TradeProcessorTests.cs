c1fb93b8235b95c99a93c39385b36f19c1173365
Mikita Puntus <150572042+mpuntus-css@users.noreply.github.com>
2024-12-18 22:22:06 -0600

    Assignment 8
    -  Made four unit tests for the public ProcessTrades() method.
    - Made ReadTradeData() public and write two unit tests for it.

--- Unit8_SRP_F24Tests/TradeProcessorTests.cs
+++ Unit8_SRP_F24Tests/TradeProcessorTests.cs
@@ -16,6 +16,8 @@ namespace SingleResponsibilityPrinciple.Tests
     {
 
 
+
+
         private int CountDbRecords()
         {
             string azureConnectString = @"Server=tcp:cis3285-sql-server.database.windows.net,1433; Initial Catalog = Unit8_TradesDatabase; Persist Security Info=False; User ID=cis3285;Password=Saints4SQL; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 60;";
@@ -35,6 +37,68 @@ namespace SingleResponsibilityPrinciple.Tests
             }
         }
 
+        //negative lot size
+        [TestMethod()]
+        public void TestNegativeNormalFiles()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Unit8_SRP_F24Tests.negativetrades.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Act
+            int countBefore = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+            //Assert
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore + 0, countAfter);
+        }
+
+        [TestMethod()]
+        public void TestTenNormalFiles()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Unit8_SRP_F24Tests.goodtrades_ten.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Act
+            int countBefore = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+            //Assert
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore, countAfter);
+        }
+
+        [TestMethod()]
+        public void TestNullNormalFiles()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Unit8_SRP_F24Tests.badtrades_null.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Act
+            int countBefore = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+            //Assert
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore, countAfter);
+        }
+
+
+
+        [TestMethod()]
+        public void TestExceptionNormalFiles()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Unit8_SRP_F24Tests.Non_Exsisting_File.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Act
+            int countBefore = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+            //Assert
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore, countAfter);
+        }
 
 
         [TestMethod()]
@@ -51,6 +115,62 @@ namespace SingleResponsibilityPrinciple.Tests
             int countAfter = CountDbRecords();
             Assert.AreEqual(countBefore, countAfter);
          }
+
+        //negative price
+        [TestMethod()]
+        public void TestOneBadNormalFiles()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Unit8_SRP_F24Tests.badtrades_one_negative.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Act
+            int countBefore = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+            //Assert
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore, countAfter);
+        }
+
+        [TestMethod()]
+        public void ReadTradeData_ValidInput_ReturnsLines()
+        {
+            // Arrange
+            string input = "PSBEYD,1000,1.51\nEURUSD,500,1.12";
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(input);
+            writer.Flush();
+            stream.Position = 0;
+
+            var tradeProcessor = new TradeProcessor();
+
+            // Act
+            var result = tradeProcessor.ReadTradeData(stream);
+
+            // Assert
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod()]
+        public void ReadTradeData_EmptyFile_ReturnsEmptyList()
+        {
+            // Arrange
+            string input = "";
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(input);
+            writer.Flush();
+            stream.Position = 0;
+
+            var tradeProcessor = new TradeProcessor();
+
+            // Act
+            var result = tradeProcessor.ReadTradeData(stream);
+
+            // Assert
+            Assert.AreEqual(0, result.Count());
+        }
 
 
 
